feb644266bf9726c2e81179ef176c370b0b34721
Jesús David Limón Hernández <jesus_limonhdz@hotmail.com>
2025-11-18 13:11:35 -0600

    first commit

---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import usePacientes from '../hooks/usePacientes'
+import { Paciente } from './Paciente';
+
+
+export const ListadoPacientes = () => {
+
+  const { pacientes } = usePacientes();
+
+  return (
+    <>
+      {pacientes.length ? (
+        <>
+          <h2 className='font-black text-3xl text-center'>Listado de Pacientes</h2>
+
+          <p className='text-xl mt-5 mb-10 text-center'>Administra tus {''} <span className='text-indigo-600 font-bold'>Pacientes y Citas</span></p>
+
+          {pacientes.map(paciente => (
+            <Paciente 
+              key={paciente._id}
+              paciente={paciente}
+            />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className='font-black text-3xl text-center'>No Hay Pacientes</h2>
+
+          <p className='text-xl mt-5 mb-10 text-center'>Comienza Agregando Pacientes {''} <span className='text-indigo-600 font-bold'> Y apareceran en este Lugar</span></p>
+        </>
+      )}
+    </>
+  )
+}
